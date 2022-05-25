78fcfaaa40ed48a6a6f72d82fc299d52825732a8
PedroPK <pedropk@gmail.com>
2022-05-25 17:09:36 -0300

    Animated Transitions
    Using JQuery to intercept the "Hamburguer" Menu Button effect
    
    When "Link-Section" has "show.bs.collapse" event triggered, we change translate parameters
    
    In CSS file, we use the "transition: 1s" to give elasticity effect

---
+++ topCasaFina-arquitetura/js/navbar-animation-fix.js
@@ -0,0 +1,7 @@
+$('#links-sections').on('show.bs.collapse', function() {
+	$('.topCasaFina-banner').css('transform', 'translate(-50%, 10%');
+});
+
+$('#links-sections').on('hide.bs.collapse', function() {
+	$('.topCasaFina-banner').css('transform', 'translate(-50%, -50%');
+});
